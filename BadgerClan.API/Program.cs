42a05d26ca79584a3b2c91a733e1a326db6b30a5
= <=>
2025-01-28 12:21:19 -0700

    Cleaned up Program.cs; added comment and endpoint name

--- BadgerClan.API/Program.cs
+++ BadgerClan.API/Program.cs
@@ -1,11 +1,7 @@
 using BadgerClan.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,35 +12,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+// Endpoint for sending a MoveRequest to BadgerClan
 app.MapPost("/", (MoveRequest request) => {
     List<Move> moves = new();
     MoveResponse response = new MoveResponse(moves);
     return response;
-});
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
 })
-.WithName("GetWeatherForecast");
+.WithName("SendMoveRequest");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
