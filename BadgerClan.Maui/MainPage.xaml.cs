c6e05630f418c55023ecab8e397f80fde1d1c383
= <=>
2025-01-29 11:28:31 -0700

    Added some of the logic for using the API

--- BadgerClan.Maui/MainPage.xaml.cs
+++ BadgerClan.Maui/MainPage.xaml.cs
@@ -1,25 +1,25 @@
-﻿namespace BadgerClan.Maui
+﻿using BadgerClan.Maui.ViewModels;
+namespace BadgerClan.Maui;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    int count = 0;
+
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
+        BindingContext = new MainPageViewModel(new HttpClient());
     }
 
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
---
+++ BadgerClan.Maui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadgerClan.Maui.ViewModels
+{
+    public partial class MainPageViewModel
+    {
+        HttpClient client;
+        public MainPageViewModel(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task GoLeft()
+        {
+            await client.GetAsync("http://localhost:port/change/direction");
+        }
+    }
+}
