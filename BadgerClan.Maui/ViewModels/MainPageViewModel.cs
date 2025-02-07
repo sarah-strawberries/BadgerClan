908b2fff7b0d36498457222a49915659408d751e
= <=>
2025-02-07 11:27:19 -0700

    Added commands in viewmodel

--- BadgerClan.Maui/ViewModels/MainPageViewModel.cs
+++ BadgerClan.Maui/ViewModels/MainPageViewModel.cs
@@ -12,15 +12,46 @@ namespace BadgerClan.Maui.ViewModels
     {
         HttpClient client;
         public ICommand MoveLeftCommand { get; }
+        public ICommand MoveUpLeftCommand { get; }
+        public ICommand MoveDownLeftCommand { get; }
+        public ICommand MoveRightCommand { get; }
+        public ICommand MoveUpRightCommand { get; }
+        public ICommand MoveDownRightCommand { get; }
         public MainPageViewModel(HttpClient client)
         {
             this.client = client;
+
             MoveLeftCommand = new Command(MoveLeft);
+            MoveUpLeftCommand = new Command(MoveUpLeft);
+            MoveDownLeftCommand = new Command(MoveDownLeft);
+            MoveRightCommand = new Command(MoveRight);
+            MoveUpRightCommand = new Command(MoveUpRight);
+            MoveDownRightCommand = new Command(MoveDownRight);
         }
 
         // Command methods
 
         public void MoveLeft()
+        {
+
+        }        
+        public void MoveUpLeft()
+        {
+
+        }        
+        public void MoveDownLeft()
+        {
+
+        }        
+        public void MoveRight()
+        {
+
+        }        
+        public void MoveUpRight()
+        {
+
+        }        
+        public void MoveDownRight()
         {
 
         }
