1fc36c5d5d6993ca548fd421cc1289f9ff139b0c
= <=>
2025-02-07 12:11:01 -0700

    Something is wrong with the CommunityToolkit.Maui package dependency

--- BadgerClan.Maui/MauiProgram.cs
+++ BadgerClan.Maui/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Mvvm;
+//using CommunityToolkit.Maui;
 
 namespace BadgerClan.Maui
 {
@@ -9,6 +11,7 @@ namespace BadgerClan.Maui
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                //.UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
