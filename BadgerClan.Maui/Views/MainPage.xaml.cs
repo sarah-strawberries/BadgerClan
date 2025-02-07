755bedcd20406a33b3db44e92cff0ec939441224
= <=>
2025-02-07 12:11:32 -0700

    Cleaned up code-behind for main page

--- BadgerClan.Maui/Views/MainPage.xaml.cs
+++ BadgerClan.Maui/Views/MainPage.xaml.cs
@@ -9,71 +9,7 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         client = new HttpClient();
-        client.BaseAddress = new Uri("http://localhost:5043"); // change localhost to Azure path when hosted
+        // client.BaseAddress = new Uri("http://localhost:5043"); // change localhost to Azure path when hosted
         BindingContext = new MainPageViewModel(client);
     }
-
-    #region Event Handlers
-    //private async void MoveLeftButton_Clicked(object sender, EventArgs e)
-    //{
-    //    await MoveLeft();
-    //}
-
-    private async void MoveUpLeftButton_Clicked(object sender, EventArgs e)
-    {
-        await MoveUpLeft();
-    }
-
-    private async void MoveUpRightButton_Clicked(object sender, EventArgs e)
-    {
-        await MoveUpRight();
-    }
-
-    private async void MoveRightButton_Clicked(object sender, EventArgs e)
-    {
-        await MoveRight();
-    }
-
-    private async void MoveDownLeft_Clicked(object sender, EventArgs e)
-    {
-        await MoveDownLeft();
-    }
-
-    private async void MoveDownRightButton_Clicked(object sender, EventArgs e)
-    {
-        await MoveDownRight();
-    }
-    #endregion
-
-    #region Methods
-    public async Task MoveLeft()
-    {
-        await client.PostAsync("MoveLeft", null);
-    }
-
-    public async Task MoveUpLeft()
-    {
-        await client.PostAsync("MoveUpLeft", null);
-    }
-
-    public async Task MoveRight()
-    {
-        await client.PostAsync("MoveRight", null);
-    }
-
-    public async Task MoveUpRight()
-    {
-        await client.PostAsync("MoveUpRight", null);
-    }
-
-    public async Task MoveDownLeft()
-    {
-        await client.PostAsync("MoveDownLeft", null);
-    }
-
-    public async Task MoveDownRight()
-    {
-        await client.PostAsync("MoveDownRight", null);
-    }
-    #endregion
 }
